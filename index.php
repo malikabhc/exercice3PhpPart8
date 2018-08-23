de1096fe14c6fa901025f955a2d8c99c1f2f1957
Malikabhc <malika.bouhcine@gmail.com>
2018-08-23 10:05:47 +0200

    exercice3-8

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+if(!empty($_POST['login']) && !empty($_POST['password'])) {
+setcookie('login', $_POST['login'], time() + 365*24*3600, null, null, false);
+setcookie('password', $_POST['password'], time() + 365*24*3600, null, null, false);
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 3/8</title>
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+            <label for="login">Identifiant</label>
+            <input type="text" name="login" id="login" placeholder="Identifiant" />
+            <label for="password">Mot de passe</label>
+            <input type="text" name="password" id="password" placeholder="Mot de passe"/>
+            <input type="submit" name="submit" value="Valider">
+        </form>
+    </body>
+</html>
